32dbd9f035d9df1b278a431df3b4453ecef09a0f
lee minseop <lms990108@gmail.com>
2024-08-07 20:50:55 +0900

    chore: 몰라

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notelens_app/common/utils/database_utils.dart';
+import 'package:notelens_app/src/ui/category/category_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,88 +16,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 0, 0)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Note Lens'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            backgroundColor: const Color.fromARGB(255, 206, 206, 206),
-            leading: IconButton(
-                icon: const Icon(Icons.sort),
-                onPressed: _incrementCounter,
-                tooltip: 'Category',
-                iconSize: 30),
-            title: Image.asset('assets/pngegg.png', width: 40, height: 40),
-            actions: [
-              IconButton(
-                onPressed: _incrementCounter,
-                icon: const Icon(Icons.settings),
-                iconSize: 30,
-              )
-            ]),
-        body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.fromLTRB(12, 20, 0, 3),
-              alignment: Alignment.bottomLeft,
-              child: const Text('Category'),
-            ),
-            Container(
-                height: 1,
-                width: double.infinity,
-                color: Colors.black,
-                margin: const EdgeInsets.fromLTRB(10, 0, 10, 0))
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 0, 0, 0)),
+          useMaterial3: true,
         ),
-        bottomNavigationBar: SizedBox(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: const Icon(
-                    Icons.help_outline_rounded,
-                    color: Color.fromARGB(255, 203, 203, 203),
-                  ),
-                  onPressed: _incrementCounter,
-                  iconSize: 40,
-                ),
-                IconButton(
-                  onPressed: _incrementCounter,
-                  icon: const Icon(
-                    Icons.add_circle_outline_rounded,
-                    color: Color.fromARGB(255, 203, 203, 203),
-                  ),
-                  iconSize: 40,
-                )
-              ],
-            )));
+        home: const CategoryPage());
   }
 }
