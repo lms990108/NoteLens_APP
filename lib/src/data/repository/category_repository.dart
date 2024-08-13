13f20dacbb6ad9c5fd05d751de74e2d10098d7b1
lee minseop <lms990108@gmail.com>
2024-08-13 23:55:16 +0900

    feat: category crud 완성, 머지 예정

--- lib/src/data/repository/category_repository.dart
+++ lib/src/data/repository/category_repository.dart
@@ -1,23 +1,32 @@
-import 'package:sqflite/sqflite.dart';
 import 'package:notelens_app/src/data/model/category.dart';
 import 'package:notelens_app/common/config/database_config.dart';
 
 class CategoryRepository {
   final DatabaseConfig _databaseConfig = DatabaseConfig(); // 싱글톤 인스턴스를 가져옵니다.
 
-  /// 데이터베이스에 새로운 카테고리를 생성하고 생성된 카테고리를 반환합니다.
+  /// 새로운 카테고리 생성
   Future<Category> createCategory(Category category) async {
     final db = await _databaseConfig.database; // 데이터베이스 객체를 가져옵니다.
     final id = await db.insert(Category.tableName, category.toMap());
     return category.copyWith(id: id);
   }
 
-  /// ID를 통해 카테고리를 조회합니다. 해당 ID의 카테고리가 존재하면 반환하고, 존재하지 않으면 null을 반환합니다.
+  /// 카테고리 조회 (ID)
   Future<Category?> getCategoryById(int id) async {
     final db = await _databaseConfig.database; // 데이터베이스 객체를 가져옵니다.
+
+    // 필드 목록을 직접 지정하여 쿼리를 실행합니다.
     final maps = await db.query(
-      Category.tableName,
-      columns: CategoryFields.values, // 필드 목록을 사용하여 쿼리할 열을 지정합니다.
+      Category.tableName, // "category"
+      columns: [
+        // 조회할 열 지정
+        CategoryFields.id,
+        CategoryFields.title,
+        CategoryFields.description,
+        CategoryFields.createdAt,
+        CategoryFields.deletedAt,
+        CategoryFields.isDeleted,
+      ], // 필요한 필드 목록 지정 (입력될 값)
       where: '${CategoryFields.id} = ?',
       whereArgs: [id],
     );
@@ -29,7 +38,7 @@ class CategoryRepository {
     }
   }
 
-  /// 데이터베이스에 있는 모든 카테고리를 조회하여 리스트로 반환합니다.
+  /// 카테고리 리스트 조회
   Future<List<Category>> getAllCategories() async {
     final db = await _databaseConfig.database; // 데이터베이스 객체를 가져옵니다.
     final maps = await db.query(Category.tableName);
@@ -48,18 +57,8 @@ class CategoryRepository {
     );
   }
 
-  /// ID를 통해 카테고리를 삭제합니다. 삭제된 행의 개수를 반환합니다.
+  /// 카테고리 soft Delete
   Future<int> deleteCategory(int id) async {
-    final db = await _databaseConfig.database; // 데이터베이스 객체를 가져옵니다.
-    return await db.delete(
-      Category.tableName,
-      where: '${CategoryFields.id} = ?',
-      whereArgs: [id],
-    );
-  }
-
-  /// 소프트 삭제를 수행합니다. isDeleted 플래그를 true로 설정하고, deletedAt 시간을 현재 시간으로 업데이트합니다. 업데이트된 행의 개수를 반환합니다.
-  Future<int> softDeleteCategory(int id) async {
     final db = await _databaseConfig.database; // 데이터베이스 객체를 가져옵니다.
     final currentDate = DateTime.now();
     return await db.update(
