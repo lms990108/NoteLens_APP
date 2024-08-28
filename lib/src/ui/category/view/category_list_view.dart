9e295a1814ae0433cc34b2093e9052fbe70294df
lee minseop <lms990108@gmail.com>
2024-08-28 22:50:57 +0900

    feat: crud 적용 초안, 테스트 아직 안함

--- lib/src/ui/category/view/category_list_view.dart
+++ lib/src/ui/category/view/category_list_view.dart
@@ -1,77 +1,112 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:notelens_app/src/ui/category/view_model/category_list_view_model.dart';
+import 'package:notelens_app/src/data/model/category.dart';
 
 class CategoryListView extends StatelessWidget {
   CategoryListView({super.key});
 
-  late final CategoryListViewModel _categoryListViewModel;
-
   @override
   Widget build(BuildContext context) {
-    _categoryListViewModel = Provider.of<CategoryListViewModel>(context, listen: false);
+    final categoryListViewModel = Provider.of<CategoryListViewModel>(context);
 
     return Scaffold(
-      appBar: _myAppBar(),
-      body: _myMainPage(),
-      bottomNavigationBar: _myBottomBar()
+      appBar: _myAppBar(categoryListViewModel),
+      body: _buildCategoryList(context, categoryListViewModel),
+      bottomNavigationBar: _myBottomBar(categoryListViewModel),
     );
   }
 
-  PreferredSizeWidget _myAppBar() {
+  PreferredSizeWidget _myAppBar(CategoryListViewModel viewModel) {
     return AppBar(
       backgroundColor: const Color.fromARGB(255, 206, 206, 206),
       leading: IconButton(
         icon: const Icon(Icons.sort),
-        onPressed: () {_categoryListViewModel.incrementCounter();},
+        onPressed: () {}, // 추가할 기능
         tooltip: 'Sidebar',
-        iconSize: 30),
-      title: Image.asset('assets/images/NoteLens.png', width: 40, height: 40),
-      actions: [ IconButton(
-        onPressed: () {_categoryListViewModel.incrementCounter();},
-        icon: const Icon(Icons.settings),
-        iconSize: 30)]
-    );
-  }
-
-  Widget _myMainPage() {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(12, 20, 0, 3),
-            alignment: Alignment.bottomLeft,
-            child: const Text('Category')
-          ),
-          Container(
-            height: 1,
-            width: double.infinity,
-            color: Colors.black,
-            margin: const EdgeInsets.fromLTRB(10, 0, 10, 0)
-          )
-        ],
+        iconSize: 30,
       ),
+      title: Image.asset('assets/images/NoteLens.png', width: 40, height: 40),
+      actions: [
+        IconButton(
+          onPressed: () {}, // 추가할 기능
+          icon: const Icon(Icons.settings),
+          iconSize: 30,
+        ),
+      ],
     );
   }
 
-  Widget _myBottomBar() {
-    return BottomAppBar(
-      color: Colors.white,
-      padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+  Widget _buildCategoryList(
+      BuildContext context, CategoryListViewModel viewModel) {
+    return FutureBuilder(
+      future: viewModel.fetchCategories(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Failed to load categories'));
+        } else {
+          return ListView.builder(
+            itemCount: viewModel.categories.length,
+            itemBuilder: (context, index) {
+              final category = viewModel.categories[index];
+              return ListTile(
+                title: Text(category.title),
+                subtitle: Text(category.description ?? ''),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        // 업데이트 로직 추가
+                        // viewModel.updateCategory(category);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        viewModel.deleteCategory(category.id!);
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+
+  Widget _myBottomBar(CategoryListViewModel viewModel) {
+    return SizedBox(
+      height: 60,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           IconButton(
-            icon: const Icon(Icons.help_outline_rounded, color: Color.fromARGB(255, 203, 203, 203)),
-            onPressed: () {_categoryListViewModel.incrementCounter();},
-            iconSize: 40
-          ),
-          const Spacer(),
-          IconButton(
-            icon: const Icon(Icons.add_circle_outline_rounded, color: Color.fromARGB(255, 203, 203, 203)),
-            onPressed: () {_categoryListViewModel.incrementCounter();},
+            icon: const Icon(Icons.help_outline_rounded,
+                color: Color.fromARGB(255, 203, 203, 203)),
+            onPressed: () {}, // 추가할 기능
             iconSize: 40,
-          )
+          ),
+          IconButton(
+            onPressed: () {
+              // 새로운 카테고리 추가 로직
+              viewModel.addCategory(
+                Category(
+                  title: 'New Category',
+                  description: 'New Description',
+                  createdAt: DateTime.now(),
+                  isDeleted: false,
+                ),
+              );
+            },
+            icon: const Icon(Icons.add_circle_outline_rounded,
+                color: Color.fromARGB(255, 203, 203, 203)),
+          ),
         ],
       ),
     );
