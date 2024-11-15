b8467b2aa31f0c5bff7d5cc35c5dcbeba7ee254f
lee minseop <lms990108@gmail.com>
2024-11-16 04:10:02 +0900

    feat: 여러 개 처리하는 초안 완성. 병합 및 통합테스트 예정

--- lib/src/ui/question/view/question_list_view.dart
+++ lib/src/ui/question/view/question_list_view.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:notelens_app/src/ui/question/view/question_answer_view.dart';
 import '../../custom/custom_appbar.dart';
 
 class QuestionListView extends StatefulWidget {
   final List<String> questions;
   final List<String> contents;
   final String originalContent;
-  final List<bool> isChecked; // 체크 상태 리스트 추가
-  final Function(int, bool) onCheckChanged; // 체크 상태 변경 시 콜백 추가
+  final List<bool> isChecked; // 체크 상태 리스트
+  final Function(int, bool) onCheckChanged; // 체크 상태 변경 콜백
 
   const QuestionListView({
-    super.key,
+    Key? key,
     required this.questions,
     required this.contents,
     required this.originalContent,
-    required this.isChecked, // 체크 상태 리스트 받기
-    required this.onCheckChanged, // 체크 상태 변경 콜백 받기
-  });
+    required this.isChecked,
+    required this.onCheckChanged,
+  }) : super(key: key);
 
   @override
   _QuestionListViewState createState() => _QuestionListViewState();
@@ -58,51 +54,6 @@ class _QuestionListViewState extends State<QuestionListView> {
           );
         },
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: ElevatedButton(
-          onPressed: () async {
-            List<String> selectedQuestions = [];
-            for (int i = 0; i < widget.isChecked.length; i++) {
-              if (widget.isChecked[i]) {
-                selectedQuestions.add(widget.contents[i]);
-              }
-            }
-
-            if (selectedQuestions.isNotEmpty) {
-              try {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (context) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                );
-
-                List<String> responses = await sendQuestionsToChatGpt(
-                    selectedQuestions, widget.originalContent);
-                print('ChatGPT Responses: $responses');
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => QuestionAnswerView(
-                      questions: selectedQuestions,
-                      answers: responses,
-                    ),
-                  ),
-                );
-              } catch (error) {
-                print('Error: $error');
-              }
-            } else {
-              print('No questions selected');
-            }
-          },
-          child: const Text('Send Selected Questions to GPT'),
-        ),
-      ),
     );
   }
 
@@ -139,47 +90,5 @@ class _QuestionListViewState extends State<QuestionListView> {
         );
       },
     );
-  }
-
-  Future<List<String>> sendQuestionsToChatGpt(
-      List<String> selectedQuestions, String originalContent) async {
-    final apiKey = dotenv.env['OPENAI_API_KEY']!;
-    final apiUrl = 'https://api.openai.com/v1/chat/completions';
-
-    List<Future<String>> apiRequests =
-        selectedQuestions.map<Future<String>>((String question) async {
-      try {
-        final response = await http.post(
-          Uri.parse(apiUrl),
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer $apiKey',
-          },
-          body: jsonEncode({
-            'model': 'gpt-3.5-turbo',
-            'messages': [
-              {
-                'role': 'user',
-                'content': '"$originalContent"의 내용 중에서, "$question" 부분에 대해 설명해줘'
-              }
-            ],
-            'max_tokens': 500,
-          }),
-        );
-
-        if (response.statusCode == 200) {
-          final utf8ResponseBody = utf8.decode(response.bodyBytes);
-          final responseData = jsonDecode(utf8ResponseBody);
-          return responseData['choices'][0]['message']['content'];
-        } else {
-          throw Exception('Failed to get response from ChatGPT API');
-        }
-      } catch (error) {
-        print('Error sending request to ChatGPT API: $error');
-        throw Exception('Error sending request to ChatGPT API');
-      }
-    }).toList();
-
-    return Future.wait(apiRequests);
   }
 }
